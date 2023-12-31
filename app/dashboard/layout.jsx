d6a134345203bc34f2452e4a11dc666e83f7623b
Rizky <rizkycyrf05@gmail.com>
2023-12-31 08:51:43 +0700

    add loading animation

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -3,13 +3,13 @@
 import Navbar from "@/components/Header/Navbar";
 import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 import { useRouter } from "next/navigation";
+import Loading from "./loading";
 
 export default function Layout({ children }) {
   const router = useRouter();
   const { isAuthenticated, isLoading } = useKindeBrowserClient();
 
-  if (isLoading)
-    return <div className="text-center text-3xl mt-20">Loading...</div>;
+  if (isLoading) return <Loading />;
 
   return isAuthenticated ? (
     // TODO: Create Authentication
